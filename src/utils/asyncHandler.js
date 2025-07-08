88bbd56480f1b9ebcad0fb4786ab40dbb47f4260
Shorya Sethia <132898518+shoryasethia@users.noreply.github.com>
2025-07-08 19:49:16 +0530

    Fix asyncHandler: added return

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler = (func) => async (req,res,next) => {
   try {
-    await func(req,res,next)
+    return await func(req,res,next)
   } 
   catch (error) {
     res.status(error.code || 500).json({
