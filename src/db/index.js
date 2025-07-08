96a7c3a6b9a5f83e6a1f0b1956b55e70f786b09c
Shorya Sethia <132898518+shoryasethia@users.noreply.github.com>
2025-07-08 16:46:03 +0530

    Add api resposne error handler

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,8 @@ import { DB_NAME } from '../constants.js'
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    console.log(`\n MONGODB connection SUCCESSFUL :: DB HOST: ${mongoose.connection.host}`)
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    console.log(`MONGODB connection SUCCESSFUL :: DB HOST: ${connectionInstance.connection.host}`)
   } 
   catch (error) {
     console.log("MONGODB connection FAILED: ", String(error))
--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import connectDB from './db/index.js'
+import app from './app.js'
 
 connectDB()
+.then(() => {
+  const PORT = process.env.PORT || 8000
+  
+  app.on("error", (error) => {
+    console.log("Server NOT running: ", String(error))
+    process.exit(1)
+  })
+
+  app.listen(PORT, () => {
+    console.log(`Server is running at PORT=${PORT}`)
+  })
+
+})
+.catch((error) => {
+  console.log("MONGODB connection FAILED: ", String(error))
+})
