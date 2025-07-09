416f654efa0338c804d23bbcbdda4af3b100472d
Shorya Sethia <132898518+shoryasethia@users.noreply.github.com>
2025-07-09 19:13:13 +0530

    Fix typo in filename: multer.middleware.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
-import { upload } from '../middlewares/mutler.middleware.js'
+import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router()
